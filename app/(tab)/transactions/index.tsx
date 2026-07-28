6cd39b63a7cdefd813fd4575c95635a340f142d0
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-07-28 16:31:00 -0600

    transaction card
    transaction card working...dummy info in transaction page used for testing

--- app/(tab)/transactions/index.tsx
+++ app/(tab)/transactions/index.tsx
@@ -1,5 +1,23 @@
+import { FlatList } from "react-native";
+import TransactionCard from "../../../components/TransactionCard";
+
+//for testing
+const TRANSACTIONS = [
+  { title: "Co-op Groceries", type: "Food", date: "Today", amount: -46.8 },
+  { title: "Paycheque", type: "Income", date: "Yesterday", amount: 980.0 },
+  { title: "Transit Pass", type: "Transport", date: "Jul 21", amount: -112 },
+];
+
 const index = () => {
-  return <div></div>;
+  return (
+    <>
+      <FlatList
+        data={TRANSACTIONS}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <TransactionCard transaction={item} />}
+      />
+    </>
+  );
 };
 
 export default index;
