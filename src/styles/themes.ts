376fc6b07986692da9c93c727cc9db86b8c1e117
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-07-28 10:52:35 -0600

    starting theme

---
+++ src/styles/themes.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    postiveText: "#3d9b6e",
+    negativeText: "#d9654c",
+    primary: "#103a33",
+    secondary: "#ff7291",
+    primaryText: "#13453d",
+    primaryTextMuted: "#6d7a73",
+    secondaryText: "#212529",
+    tertiaryText: "#e25272",
+    quaternaryText: "#ffffff",
+    quanternaryTextMuted: "#cbd4d2",
+    background: "#fbf7f1",
+  },
+};
